c45420dd76af1aec47750c3c59a321342fe89c4e
popochoo <polshamangood@gmail.com>
2024-07-22 11:28:04 +0300

    created locales

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    form: {
+      submit: 'Add',
+      loading: 'Waiting',
+    },
+    feeds: 'Feeds',
+    posts: 'Posts',
+    preview: 'Preview',
+    success: 'RSS uploaded successfully',
+
+    errors: {
+      default: 'Incorrect data',
+      url: 'The link must be a valid URL',
+      exist: 'RSS already exists',
+      noRSS: 'No valid RSS founded',
+      networkError: 'Network Error',
+    },
+  },
+};
