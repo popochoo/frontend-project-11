77d260cf3002521c78f8f9fd9475c3d56614a60e
popochoo <polshamangood@gmail.com>
2024-07-22 11:27:36 +0300

    created utils

---
+++ src/utils/parser.js
@@ -0,0 +1,30 @@
+const domParser = new DOMParser();
+
+export default (responseData) => {
+  const xmlDocument = domParser.parseFromString(responseData, 'text/xml');
+  const rootTagName = xmlDocument.documentElement.tagName.toLocaleLowerCase();
+
+  if (rootTagName !== 'rss') throw new Error('noRSS');
+
+  const channel = xmlDocument.querySelector('channel');
+  const channelTitle = xmlDocument.querySelector('channel title').textContent;
+  const channelDescription = xmlDocument.querySelector('channel description').textContent;
+
+  const itemElements = channel.getElementsByTagName('item');
+
+  const items = [...itemElements].map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('channel link').textContent;
+
+    return { title, description, link };
+  });
+
+  const parsedData = {
+    title: channelTitle,
+    description: channelDescription,
+    items,
+  };
+
+  return parsedData;
+};
